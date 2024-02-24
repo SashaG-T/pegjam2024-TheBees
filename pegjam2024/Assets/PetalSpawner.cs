c37467432268f80931336a77648821fbc64c8ce6
Seanmus <seanpiche718@gmail.com>
2024-02-24 13:13:49 -0600

    Updated flower petal spawner to randomly spawn in a ring around the flowers center

--- pegjam2024/Assets/PetalSpawner.cs
+++ pegjam2024/Assets/PetalSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PetalSpawner : MonoBehaviour
@@ -11,7 +12,9 @@ public class PetalSpawner : MonoBehaviour
                                 new Color(1f, 0.6627f, 0.2588f),
                                 new Color(0.6196f, 0.9921f, 1f),
                                 new Color(0.6275f, 0.6196f, 1f),
-                                new Color(1f, 0.9373f, 0.2588f)
+                                new Color(1f, 0.9373f, 0.2588f),
+                                new Color(0.2667f, 0.25882f, 1f),
+                                new Color(0.6196f, 0.9922f, 1f)
     };
 
     [SerializeField]
@@ -26,12 +29,47 @@ public class PetalSpawner : MonoBehaviour
     /// </summary>
     void Start()
     {
-        int colorIndex = Random.Range(0, colors.Length);
-        for (int i = 0; i < numberOfPetals; i++) 
+        int j = 0;
+        if (numberOfPetals > 8)
         {
+            numberOfPetals = 8;
+        }
+        int difFromEight = 8 - numberOfPetals;
+        List<int> petalsToSkip = new List<int>();
+        while (j < difFromEight)
+        {
+            bool isUniqueIndex = true;
+            int petalToSkipIndex = Random.Range(0, 7);
+            foreach(int i in petalsToSkip)
+            {
+                if (i == petalToSkipIndex)
+                {
+                    isUniqueIndex = false;
+                    continue;
+                }
+            }
+            if (!isUniqueIndex) { continue; }
+            petalsToSkip.Add(petalToSkipIndex);
+            j++;
+        }
+        Debug.Log(petalsToSkip);
+        int colorIndex = Random.Range(0, colors.Length);
+        for (int i = 0; i < 8; i++) 
+        {
+            bool isSkipableIndex = false;
+            foreach(int p in petalsToSkip)
+            {
+                if (p == i)
+                {
+                    isSkipableIndex = true;
+                    break;
+                }
+            }
+            if(isSkipableIndex) { continue; }
+
             GameObject newPetal = Instantiate(petal, parentTransform);
             newPetal.transform.localEulerAngles = new Vector3(90 , 0, 0);
-            newPetal.transform.Rotate(0, (360 / numberOfPetals) * i, 0);
+            newPetal.transform.Rotate(0, (360 / 8) * i, 0);
             newPetal.gameObject.GetComponent<Renderer>().material.color = colors[colorIndex];
             newPetal.transform.localPosition = Vector3.zero;
 
